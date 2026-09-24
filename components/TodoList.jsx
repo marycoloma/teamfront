457753a2faaf0431f1b281e76d1a4208850e83d7
Munachi Amadike <Munachi.Amadike@sait.ca>
2026-09-24 09:07:05 -0600

    changed title to text

--- components/TodoList.jsx
+++ components/TodoList.jsx
@@ -1,31 +1,29 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import { TodoItem } from "./TodoItem";
 import { colors } from "../assets/theme";
-
+ 
 export function TodoList({ todos, onDeleteItem }) {
   return (
-    <View style={styles.todoListContainer}>
-      <Text style={styles.todoSectionTitle}>Your Todos</Text>
-      <FlatList
+<View style={styles.todoListContainer}>
+<Text style={styles.todoSectionTitle}>Your Todos</Text>
+<FlatList
         alwaysBounceVertical={false}
         keyExtractor={(item) => item.id}
         data={todos}
         renderItem={(itemData) => {
           return (
-            // TODO: Update component props
-            <TodoItem
+<TodoItem
               id={itemData.item.id}
-              title={itemData.item.title}
-              description={itemData.item.description}
+              text={itemData.item.title}
               onDeleteItem={onDeleteItem}
             />
           );
         }}
       />
-    </View>
+</View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
   todoListContainer: {
     marginTop: 20,
